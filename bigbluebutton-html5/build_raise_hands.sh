575da24b1317efc5a3c99f36932f6ba425e42779
Gammelalf <31348226+gammelalf@users.noreply.github.com>
2020-10-24 21:29:58 +0200

    Added check for dev user

--- bigbluebutton-html5/build_raise_hands.sh
+++ bigbluebutton-html5/build_raise_hands.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-echo "Ensuring correct cwd"
-cd /home/dev/dev/bigbluebutton/bigbluebutton-html5
+if [ $(whoami) == "dev" ]
+then
+	echo "Ensuring correct cwd"
+	cd /home/dev/dev/bigbluebutton/bigbluebutton-html5
 
-echo "Building into ./build/"
-meteor build --server-only ./build/
+	echo "Building into ./build/"
+	meteor build --server-only build/
+else
+	echo "Please call me as dev"
+fi

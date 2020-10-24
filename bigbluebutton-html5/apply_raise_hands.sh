1619cb2c496674eafb57f34624ccec941ff899b0
Gammelalf <31348226+gammelalf@users.noreply.github.com>
2020-10-24 21:28:46 +0200

    Added check for root user and backup of old bundle

--- bigbluebutton-html5/apply_raise_hands.sh
+++ bigbluebutton-html5/apply_raise_hands.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-echo "Overwritting html5 module with custom build"
-sudo tar -xf bigbluebutton-html5.tar.gz -C /usr/share/meteor
+if [ $(whoami) == "root" ]
+then
+	echo "Backup current bundle to bundle.bkp"
+	cp -R /usr/share/meteor/bundle /usr/share/meteor/bundle.bkp
 
-echo "Ensuring correct ownership"
-chown meteor:meteor /usr/share/meteor/bundle -R
+	echo "Overwrite html5 module with custom build"
+	tar -xf bigbluebutton-html5.tar.gz -C /usr/share/meteor
 
-echo "Restarting bbb-html5 service"
-sudo systemctl restart bbb-html5
+	echo "Ensure correct ownership"
+	chown meteor:meteor /usr/share/meteor/bundle -R
+
+	echo "Restart bbb-html5 service"
+	systemctl restart bbb-html5
+else
+	echo "Please call me as root"
+fi

b203ad9fd28961f7808003bfe918c872eb91b93b
gammelalf <31348226+gammelalf@users.noreply.github.com>
2021-02-10 03:13:58 +0100

    Comment out the js hack

--- bigbluebutton-html5/apply_config.py
+++ bigbluebutton-html5/apply_config.py
@@ -10,12 +10,11 @@ if __name__ == "__main__":
 	with open(file_) as f:
 		content = f.read()
 
-	content = re.sub(
-		r"    sipjsHackViaWs: false",
-		r"    sipjsHackViaWs: true",
-		content
-
-	)
+	#content = re.sub(
+	#	r"    sipjsHackViaWs: false",
+	#	r"    sipjsHackViaWs: true",
+	#	content
+	#)
 
 	content = re.sub(
 		r"        raiseHandAudioAlerts: (true|false)\n"
